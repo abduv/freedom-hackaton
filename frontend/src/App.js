0d6ec14db673c7954346b45a9c7f7e202406b425
Castle1919 <nursultan12369@gmail.com>
2024-11-09 10:58:47 +0500

    frontend:update main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { Header } from "./components/header";
-import Main  from "./components/main"
+import { Main2 } from "./components/main2";
 function App() {
   return (
     <div>
-      <Header />
       <Routes>
-        <Route exact path="/" element={<Main />} />
-        <Route exact path="/main" element={<Main />} />
+        <Route exact path="/" element={<Main2 />} />
+        <Route exact path="/main" element={<Main2 />} />
       </Routes>
     </div>
   );
